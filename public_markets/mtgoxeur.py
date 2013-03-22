033fe8df6e18ffa9b1ffc0b82bfcf7eaae58c2c8
Kyle Fitzsimmons <kafitz22@gmail.com>
2013-03-22 09:59:07 -0400

    Synced changes from maxme's bitcoin-arbitrage master project

--- public_markets/mtgoxeur.py
+++ public_markets/mtgoxeur.py
@@ -8,15 +8,19 @@ class MtGoxEUR(Market):
     def __init__(self):
         super(MtGoxEUR, self).__init__("EUR")
         self.update_rate = 25
+        self.depth = {'asks': [{'price': 0, 'amount': 0}], 'bids': [{'price': 0, 'amount': 0}]}
 
     def update_depth(self):
         res = urllib2.urlopen('http://data.mtgox.com/api/1/BTCEUR/depth/fetch')
         jsonstr = res.read()
         try:
-            depth = json.loads(jsonstr)
-            self.depth = self.format_depth(depth)
+            data = json.loads(jsonstr)
         except Exception:
-            logging.warn("Can't parse json:" + jsonstr)
+            logging.error("%s - Can't parse json: %s" % (self.name, jsonstr))
+        if data["result"] == "success":
+            self.depth = self.format_depth(data["return"])
+        else:
+            logging.error("%s - fetched data error" % (self.name))
 
     def sort_and_format(self, l, reverse=False):
         # sort list: for each dict in input list, get price key and sort by that
@@ -24,13 +28,13 @@ class MtGoxEUR(Market):
         ret = []
         for i in l:
             # create a return a list of dicts sorted according to bid/ask w/ only the price and volume
-            ret.append({'price': float(i.get('price')), 'amount': float(i.get('amount'))})
+            ret.append({'price': float(i['price']), 'amount': float(i['amount'])})
         return ret
 
     def format_depth(self, depth):
         # returns a dict for comparison against other exchanges in arbitrage.py:tick()
-        bids = self.sort_and_format(depth['return']['bids'], True)
-        asks = self.sort_and_format(depth['return']['asks'], False)
+        bids = self.sort_and_format(depth['bids'], True)
+        asks = self.sort_and_format(depth['asks'], False)
         return {'asks': asks, 'bids': bids}
 
 if __name__ == "__main__":
