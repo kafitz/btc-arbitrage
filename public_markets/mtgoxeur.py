66dc8036dfe1bd9de308ad924ddf48650da76da9
Kyle Fitzsimmons <kafitz22@gmail.com>
2013-03-21 21:24:46 -0400

    Updated mt.gox REST api modules
    Added sort functionality to USD module so that it can properly be used
    by arbitrage.py script; updated EUR script to new version of API

--- public_markets/mtgoxeur.py
+++ public_markets/mtgoxeur.py
@@ -10,7 +10,7 @@ class MtGoxEUR(Market):
         self.update_rate = 25
 
     def update_depth(self):
-        res = urllib2.urlopen('https://data.mtgox.com/api/0/data/getDepth.php?Currency=EUR')
+        res = urllib2.urlopen('http://data.mtgox.com/api/1/BTCEUR/depth/fetch')
         jsonstr = res.read()
         try:
             depth = json.loads(jsonstr)
@@ -19,15 +19,18 @@ class MtGoxEUR(Market):
             logging.warn("Can't parse json:" + jsonstr)
 
     def sort_and_format(self, l, reverse=False):
-        l.sort(key=lambda x: float(x[0]), reverse=reverse)
-        r = []
+        # sort list: for each dict in input list, get price key and sort by that
+        l.sort(key=lambda x: float(x.get('price')), reverse=reverse)
+        ret = []
         for i in l:
-            r.append({'price': float(i[0]), 'amount': float(i[1])})
-        return r
+            # create a return a list of dicts sorted according to bid/ask w/ only the price and volume
+            ret.append({'price': float(i.get('price')), 'amount': float(i.get('amount'))})
+        return ret
 
     def format_depth(self, depth):
-        bids = self.sort_and_format(depth['bids'], True)
-        asks = self.sort_and_format(depth['asks'], False)
+        # returns a dict for comparison against other exchanges in arbitrage.py:tick()
+        bids = self.sort_and_format(depth['return']['bids'], True)
+        asks = self.sort_and_format(depth['return']['asks'], False)
         return {'asks': asks, 'bids': bids}
 
 if __name__ == "__main__":
--- public_markets/mtgoxusd.py
+++ public_markets/mtgoxusd.py
@@ -19,15 +19,19 @@ class MtGoxUSD(Market):
             logging.warn("Can't parse json:" + jsonstr)
 
     def sort_and_format(self, l, reverse=False):
-    	ret = []
+        # sort list: for each dict in input list, get price key and sort by that
+    	l.sort(key=lambda x: float(x.get('price')), reverse=reverse)
+        ret = []
     	for i in l:
-    		ret.append({'price': i.get('price'), 'amount': i.get('amount')})
+            # create a return a list of dicts sorted according to bid/ask w/ only the price and volume
+    		ret.append({'price': float(i.get('price')), 'amount': float(i.get('amount'))})
     	return ret
 
 
     def format_depth(self, depth):
-        bids = self.sort_and_format(depth['return']['bids'])
-        asks = self.sort_and_format(depth['return']['asks'])
+        # returns a dict for comparison against other exchanges in arbitrage.py:tick()
+        bids = self.sort_and_format(depth['return']['bids'], True)
+        asks = self.sort_and_format(depth['return']['asks'], False)
         return {'asks': asks, 'bids': bids}
 
 if __name__ == "__main__":
